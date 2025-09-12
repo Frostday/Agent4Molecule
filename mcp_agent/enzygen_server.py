c9eeafec37ca2400768cda9a7c776d315e0ee125
Dhruv Garg <dgarg2@br013.ib.bridges2.psc.edu>
2025-09-12 19:53:56 -0400

    enzygen does not use substrate during test time

--- mcp_agent/enzygen_server.py
+++ mcp_agent/enzygen_server.py
@@ -62,12 +62,12 @@ def build_enzygen_input(
     motif_coord: Annotated[list[list[float]], Field(description="Coordinates of the motif")],
     recommended_length: Annotated[int, Field(description="Recommended length of the enzyme to be generated")],
     ref_pdb_chain: Annotated[str, Field(description="Reference PDB chain")] = "AAAA.A",
-    substrate_file: Annotated[str, Field(description="Substrate file")] = None,
+    # substrate_file: Annotated[str, Field(description="Substrate file")] = None,
 ) -> str:
     file_name = os.path.join(ENZYGEN_PATH, "data/input.json")
     data = {}
     indices = ",".join([str(i) for i in sorted(motif_indices)])+"\n"
-    pdb, ec4, substrate = ref_pdb_chain, enzyme_family, substrate_file
+    pdb, ec4 = ref_pdb_chain, enzyme_family
     seq, coord = "", ""
     idx = 0
     for i in range(recommended_length):
@@ -90,8 +90,8 @@ def build_enzygen_input(
             }
         }
     }
-    if substrate:
-        data[".".join(enzyme_family.split(".")[:-1])]["test"]["substrate"] = [substrate]
+    # if substrate_file:
+    #     data[".".join(enzyme_family.split(".")[:-1])]["test"]["substrate"] = [substrate_file]
     with open(file_name, 'w') as f:
         f.write(json.dumps(data, indent=4))
     return "Created input file for Enzygen: " + file_name
@@ -155,7 +155,7 @@ if __name__ == "__main__":
     # print(find_enzyme_category_using_keywords(["adenylate", "cyclase", "adenylylcyclase"]))
     # print(find_mined_motifs_enzyme_category("4.6.1.1", start_index=0, end_index=2))
     # cleanup()
-    # print(build_enzygen_input("4.6.1.1", [7, 9, 13, 16, 19, 20, 22, 28, 58, 59, 63, 67, 75, 82, 84, 90, 115, 117, 119, 123, 130, 138, 143, 149, 154, 158, 167], ['R', 'I', 'F', 'I', 'F', 'T', 'M', 'S', 'G', 'D', 'A', 'A', 'E', 'A', 'A', 'A', 'R', 'G', 'H', 'A', 'S', 'A', 'V', 'L', 'A', 'I', 'Y'], [[43.506, -6.758, 46.718], [38.345, -4.765, 44.212], [27.09, 0.648, 48.385], [17.123, 3.522, 50.054], [11.402, 2.391, 54.192], [10.907, 3.571, 57.767], [6.486, 3.519, 54.572], [5.508, -5.321, 61.885], [19.634, -1.77, 57.845], [18.156, -1.1, 54.433], [28.436, -5.703, 47.564], [36.463, -11.125, 46.134], [34.608, -7.277, 34.653], [24.991, -3.646, 35.749], [23.48, -0.824, 40.253], [13.852, -3.122, 40.484], [21.286, 5.9, 46.057], [28.037, 4.6, 45.147], [34.503, 2.711, 43.701], [41.431, -2.971, 49.986], [38.431, -12.864, 66.38], [36.711, -5.303, 52.874], [35.782, 1.19, 51.082], [30.113, 8.463, 47.15], [21.426, 12.621, 44.494], [24.48, 6.126, 40.615], [35.757, 1.433, 32.015]], 198, "1wc3.A", substrate_file="CHEBI_57540.sdf"))
-    # print(build_enzygen_input("4.6.1.1", [0, 1, 4], ['D', 'I', 'G'], [[1.0, 1.0, 1.0], [2.0, 2.0, 2.0], [3.0, 3.0, 3.0]], 5, substrate_file="CHEBI_57540.sdf"))
+    # print(build_enzygen_input("4.6.1.1", [7, 9, 13, 16, 19, 20, 22, 28, 58, 59, 63, 67, 75, 82, 84, 90, 115, 117, 119, 123, 130, 138, 143, 149, 154, 158, 167], ['R', 'I', 'F', 'I', 'F', 'T', 'M', 'S', 'G', 'D', 'A', 'A', 'E', 'A', 'A', 'A', 'R', 'G', 'H', 'A', 'S', 'A', 'V', 'L', 'A', 'I', 'Y'], [[43.506, -6.758, 46.718], [38.345, -4.765, 44.212], [27.09, 0.648, 48.385], [17.123, 3.522, 50.054], [11.402, 2.391, 54.192], [10.907, 3.571, 57.767], [6.486, 3.519, 54.572], [5.508, -5.321, 61.885], [19.634, -1.77, 57.845], [18.156, -1.1, 54.433], [28.436, -5.703, 47.564], [36.463, -11.125, 46.134], [34.608, -7.277, 34.653], [24.991, -3.646, 35.749], [23.48, -0.824, 40.253], [13.852, -3.122, 40.484], [21.286, 5.9, 46.057], [28.037, 4.6, 45.147], [34.503, 2.711, 43.701], [41.431, -2.971, 49.986], [38.431, -12.864, 66.38], [36.711, -5.303, 52.874], [35.782, 1.19, 51.082], [30.113, 8.463, 47.15], [21.426, 12.621, 44.494], [24.48, 6.126, 40.615], [35.757, 1.433, 32.015]], 198, "1wc3.A"))
+    # print(build_enzygen_input("4.6.1.1", [0, 1, 4], ['D', 'I', 'G'], [[1.0, 1.0, 1.0], [2.0, 2.0, 2.0], [3.0, 3.0, 3.0]], 5))
     # print(run_enzygen(f"{ENZYGEN_PATH}/data/input.json"))
     # print(run_enzygen(f"{ENZYGEN_PATH}/data/test_2.json"))
