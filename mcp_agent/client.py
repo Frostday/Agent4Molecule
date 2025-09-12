44de8b5318ebd89ebc55b178cd3836f29caa3543
Dhruv Garg <dgarg2@br014.ib.bridges2.psc.edu>
2025-09-12 18:03:24 -0400

    enzygen flexible prompting implemented

--- mcp_agent/client.py
+++ mcp_agent/client.py
@@ -136,20 +136,12 @@ class MCPClient:
         print("\nMCP Client Started!")
         print("Type your queries or 'quit' to exit.")
         
-        while True:
-            try:
-                # query = input("\nQuery: ").strip()
-                # query = "Design an enzyme using the given data:\\n- enzyme family = \"4.6.1\"\\n- EC4 category = \"4.6.1.1\"\\n- Information about the motif:\\n    - Amino acid = \"I\", Coordinates = [1.0, 1.0, 1.0], Index = 1\\n    - Amino acid = \"G\", Coordinates = [2.0, 1.8, 1.5], Index = 4\\n    - Amino acid = \"D\", Coordinates = [1.1, 1.2, 2.0], Index = 0\\n- PDB file = \"5cxl.A\"\\n- Recommended length = 20".replace("\\n", "\n")
-                query = "Design a heme binding protein using the given data: \\n- Input PDB with protein and ligand: \"/ocean/projects/cis240137p/dgarg2/github/heme_binder_diffusion/input/7o2g_HBA.pdb\"\\n- Ligand name: \"HBA\"\\n- Parameters file: \"/ocean/projects/cis240137p/dgarg2/github/heme_binder_diffusion/theozyme/HBA/HBA.params\"\\n- CST file: \"/ocean/projects/cis240137p/dgarg2/github/heme_binder_diffusion/theozyme/HBA/HBA_CYS_UPO.cst\"\\n- ligand atoms that should be excluded from clashchecking because they are flexible: \"O1 O2 O3 O4 C5 C10\"\\n- ligand atoms that need to be more exposed and the required SASA for those atoms: \"C45 C46 C47\" and SASA should be 10.0\\n- amino acids should be excluded from consideration when generating protein sequences: \"CM\"\\n- ligand atom used for aligning the rotamers: \"N1\", \"N2\", \"N3\", \"N4\"\\nHere are some properties you should try to obtain:\\n- SASA <= 0.3\\n- RMSD <= 5\\n- LDDT >= 80\\n- Terminal residue limit < 15\\n- Radius of gyration limit for protein compactness <= 30\\n- all_cst <= 1.5\\n- CMS per atom >= 3.0\\n- CYS atom is A15".replace("\\n", "\n")
-                
-                if query.lower() == 'quit':
-                    break
-                
-                message_history = await self.process_query(query)
-                print(message_history)
-                    
-            except Exception as e:
-                print(f"\nError: {str(e)}")
+        query = input("\nQuery: ").strip()
+        # query = "Design an enzyme that functions as an adenylate-processing protein, acting like a cyclase to transform ATP into 3’,5’-cyclic AMP while releasing pyrophosphate. The enzyme should resemble known adenylylcyclases in structure and activity, and be capable of catalyzing the formation of cyclic AMP as a signaling molecule."
+        # query = "Design a heme binding protein using the given data: \\n- Input PDB with protein and ligand: \"/ocean/projects/cis240137p/dgarg2/github/heme_binder_diffusion/input/7o2g_HBA.pdb\"\\n- Ligand name: \"HBA\"\\n- Parameters file: \"/ocean/projects/cis240137p/dgarg2/github/heme_binder_diffusion/theozyme/HBA/HBA.params\"\\n- CST file: \"/ocean/projects/cis240137p/dgarg2/github/heme_binder_diffusion/theozyme/HBA/HBA_CYS_UPO.cst\"\\n- ligand atoms that should be excluded from clashchecking because they are flexible: \"O1 O2 O3 O4 C5 C10\"\\n- ligand atoms that need to be more exposed and the required SASA for those atoms: \"C45 C46 C47\" and SASA should be 10.0\\n- amino acids should be excluded from consideration when generating protein sequences: \"CM\"\\n- ligand atom used for aligning the rotamers: \"N1\", \"N2\", \"N3\", \"N4\"\\nHere are some properties you should try to obtain:\\n- SASA <= 0.3\\n- RMSD <= 5\\n- LDDT >= 80\\n- Terminal residue limit < 15\\n- Radius of gyration limit for protein compactness <= 30\\n- all_cst <= 1.5\\n- CMS per atom >= 3.0\\n- CYS atom is A15".replace("\\n", "\n")
+        
+        message_history = await self.process_query(query)
+        # print(message_history)
     
     async def cleanup(self):
         """Clean up resources"""
--- mcp_agent/enzygen_server.py
+++ mcp_agent/enzygen_server.py
@@ -4,6 +4,9 @@ from pydantic import Field
 import json
 import subprocess
 import time
+import pandas as pd
+import numpy as np
+import glob
 
 from run_utils import extract_job_id
 
@@ -18,33 +21,66 @@ mcp = FastMCP("enzygen")
 
 
 @mcp.tool()
+def find_enzyme_category_using_keywords(
+    keywords: Annotated[list[str], Field(description="Keywords containing the enzyme related information like chemical compound names, gene names etc.")],
+) -> str:
+    df = pd.read_csv("data/enzymes.txt", sep="\t", header=None, names=["EC4", "description"])
+    matches = [df['description'].str.contains(kw, case=False) for kw in keywords]
+    match_counts = pd.concat(matches, axis=1).sum(axis=1)
+    df['match_count'] = match_counts
+    result = df[df['match_count'] > 0].sort_values(by='match_count', ascending=False)
+    return "Top 5 results:\n\n" + result.head(5).to_csv(index=False)
+
+
+@mcp.tool()
+def find_mined_motifs_enzyme_category(
+    enzyme_category: Annotated[str, Field(description="Enzyme category (e.g. 4.6.1.1)")],
+    start_index: Annotated[int, Field(description="Start index (suggestion: only extract 2-5 at a time)")] = 0,
+    end_index: Annotated[int, Field(description="End index (suggestion: only extract 2-5 at a time)")] = 5,
+) -> str:
+    if end_index < start_index:
+        return "End index should be greater than start index"
+    with open("data/mined_motifs.json") as f:
+        data = json.load(f)
+    if enzyme_category not in data.keys():
+        return "No motif information available for the enzyme category - try another EC4 category or ask the user for motif information"
+    data = data[enzyme_category]
+    options = []
+    if end_index > len(data):
+        return "Number of mined motifs is less than the end index (total: {})".format(len(data))
+    for i, d in enumerate(data[start_index:end_index]):
+        text = f"- Test {i+1}\n\t- Motif Indices: {d['motif']}\n\t- Motif Sequence: {np.array(d['seq'])[d['motif']].tolist()}\n\t- Motif coordinates: {np.array(d['coor'])[d['motif']].tolist()}\n\t- Recommended length: {len(d['seq'])}\n\t- Reference PDB: {d['pdb']}"
+        options.append(text)
+    return f"Total motif options: {len(data)}\n\nHere are some mined motifs for the enzyme family {enzyme_category}:\n\n" + "\n".join(options)
+
+
+@mcp.tool()
 def build_enzygen_input(
-    enzyme_family: Annotated[str, Field(description="Enzyme family name")],
-    motif_seq: Annotated[str, Field(description="Sequence of the motif")],
-    motif_coord: Annotated[list[float], Field(description="Coordinates of the motif")],
+    enzyme_family: Annotated[str, Field(description="Enzyme family of the enzyme to be generated (EC4 category e.g. 1.1.1.1)")],
     motif_indices: Annotated[list[int], Field(description="Indices of the motif")],
-    pdb_file: Annotated[str, Field(description="PDB file")],
-    ec4_category: Annotated[str, Field(description="EC4 category")],
-    recommended_length: Annotated[int, Field(description="Recommended length")],
+    motif_seq: Annotated[list[str], Field(description="Sequence of the motif")],
+    motif_coord: Annotated[list[list[float]], Field(description="Coordinates of the motif")],
+    recommended_length: Annotated[int, Field(description="Recommended length of the enzyme to be generated")],
+    ref_pdb_chain: Annotated[str, Field(description="Reference PDB chain")] = "AAAA.A",
     substrate_file: Annotated[str, Field(description="Substrate file")] = None,
 ) -> str:
     file_name = os.path.join(ENZYGEN_PATH, "data/input.json")
     data = {}
     indices = ",".join([str(i) for i in sorted(motif_indices)])+"\n"
-    pdb, ec4, substrate = pdb_file, ec4_category, substrate_file
+    pdb, ec4, substrate = ref_pdb_chain, enzyme_family, substrate_file
     seq, coord = "", ""
     idx = 0
     for i in range(recommended_length):
         if i in motif_indices:
             idx = motif_indices.index(i)
             seq += motif_seq[idx]
-            coord += ",".join([str(i) for i in motif_coord[idx*3:idx*3+3]])+","
+            coord += ",".join([str(i) for i in motif_coord[idx]])+","
         else:
             seq += "A"
             coord += "0.0,0.0,0.0,"
     coord = coord[:-1]
     data = {
-        enzyme_family: {
+        ".".join(enzyme_family.split(".")[:-1]): {
             "test": {
                 "seq": [seq],
                 "coor": [coord],
@@ -55,7 +91,7 @@ def build_enzygen_input(
         }
     }
     if substrate:
-        data[enzyme_family]["test"]["substrate"] = [substrate]
+        data[".".join(enzyme_family.split(".")[:-1])]["test"]["substrate"] = [substrate]
     with open(file_name, 'w') as f:
         f.write(json.dumps(data, indent=4))
     return "Created input file for Enzygen: " + file_name
@@ -86,7 +122,7 @@ def run_enzygen(input_json: Annotated[str, Field(description="Location of script
         if job_id not in str(output):
             break
         print("Job still running...")
-        time.sleep(300)
+        time.sleep(60)
 
     output_preds = []
     for enzyme_family in os.listdir(f"{ENZYGEN_PATH}/outputs"):
@@ -99,7 +135,9 @@ def run_enzygen(input_json: Annotated[str, Field(description="Location of script
         logs = f.read()
     with open(f"{ENZYGEN_PATH}/output.err", "r") as f:
         errors = f.read()
-    return "Predicted structure(s) from EnzyGen:\n\n----------\n" + "\n----------\n----------\n".join(output_preds) + "\n----------\n\nLog File:\n\n----------\n" + logs + "\n----------\n\nError File:\n\n----------\n" + errors + "\n----------\n"
+    
+    # return "Predicted structure(s) from EnzyGen:\n\n----------\n" + "\n----------\n----------\n".join(output_preds) + "\n----------\n\nLog File:\n\n----------\n" + logs + "\n----------\n\nError File:\n\n----------\n" + errors + "\n----------\n"
+    return f"EnzyGen Finished Successfully\nPredicted structure(s) from EnzyGen: {glob.glob(f"{ENZYGEN_PATH}/outputs/*/pred_pdbs/*.pdb")}\n\nLog File:\n\n----------\n" + logs + "\n----------\n\nError File:\n\n----------\n" + errors + "\n----------\n"
 
 
 def cleanup():
@@ -113,7 +151,11 @@ def cleanup():
 
 if __name__ == "__main__":
     mcp.run(transport='stdio')
+    # print(find_enzyme_category_using_keywords(["oxidase", "D-ARABINONO-1,4-LACTONE"]))
+    # print(find_enzyme_category_using_keywords(["adenylate", "cyclase", "adenylylcyclase"]))
+    # print(find_mined_motifs_enzyme_category("4.6.1.1", start_index=0, end_index=2))
     # cleanup()
-    # print(get_motif_sequence("4.6.1", "DIG", [1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 3.0, 3.0, 3.0], [0, 1, 4], "5cxl.A", "4.6.1.1", 5, "CHEBI_57540.sdf"))
+    # print(build_enzygen_input("4.6.1.1", [7, 9, 13, 16, 19, 20, 22, 28, 58, 59, 63, 67, 75, 82, 84, 90, 115, 117, 119, 123, 130, 138, 143, 149, 154, 158, 167], ['R', 'I', 'F', 'I', 'F', 'T', 'M', 'S', 'G', 'D', 'A', 'A', 'E', 'A', 'A', 'A', 'R', 'G', 'H', 'A', 'S', 'A', 'V', 'L', 'A', 'I', 'Y'], [[43.506, -6.758, 46.718], [38.345, -4.765, 44.212], [27.09, 0.648, 48.385], [17.123, 3.522, 50.054], [11.402, 2.391, 54.192], [10.907, 3.571, 57.767], [6.486, 3.519, 54.572], [5.508, -5.321, 61.885], [19.634, -1.77, 57.845], [18.156, -1.1, 54.433], [28.436, -5.703, 47.564], [36.463, -11.125, 46.134], [34.608, -7.277, 34.653], [24.991, -3.646, 35.749], [23.48, -0.824, 40.253], [13.852, -3.122, 40.484], [21.286, 5.9, 46.057], [28.037, 4.6, 45.147], [34.503, 2.711, 43.701], [41.431, -2.971, 49.986], [38.431, -12.864, 66.38], [36.711, -5.303, 52.874], [35.782, 1.19, 51.082], [30.113, 8.463, 47.15], [21.426, 12.621, 44.494], [24.48, 6.126, 40.615], [35.757, 1.433, 32.015]], 198, "1wc3.A", substrate_file="CHEBI_57540.sdf"))
+    # print(build_enzygen_input("4.6.1.1", [0, 1, 4], ['D', 'I', 'G'], [[1.0, 1.0, 1.0], [2.0, 2.0, 2.0], [3.0, 3.0, 3.0]], 5, substrate_file="CHEBI_57540.sdf"))
     # print(run_enzygen(f"{ENZYGEN_PATH}/data/input.json"))
     # print(run_enzygen(f"{ENZYGEN_PATH}/data/test_2.json"))
