6e915c74ba6e2899170bf0d02e96af82146281d0
Dhruv Garg <dgarg2@br014.ib.bridges2.psc.edu>
2025-10-11 12:49:07 -0400

    heme binder pipeline working

--- mcp_agent/prompts.py
+++ mcp_agent/prompts.py
@@ -4,16 +4,16 @@ A user will ask you to run a certain pipeline, or parts of pipelines, and provid
 Based on what the user requests, determine the right sequence of tools to run, and if necessary, use the output from one tool as the input for the next one.                            
 If the user provides values for parameters, use those instead of the default ones.
 
-The heme binder pipeline follows this sequence: Run RF Diffusion -> Analyze RF Diffusion Outputs -> Run ProteinMPNN -> Run AF2 -> Analyze AF2 Outputs -> Run LigandMPNN -> Analyze LigandMPNN Outputs -> Run LigandMPNN on 2nd layer residues -> Run AF2 -> Analyze AF2 Outputs -> Run FastRelax -> Analyze FastRelax Outputs
-At any step, if the model does not meet the required criteria (e.g., SASA, RMSD, LDDT, etc.), try rerunning the previous stages with improved parameters.
-Do not continue the pipeline with number of good structure(s) being 0 (try making thresholds more flexible if required).
-
 Suggestions for running the enzyme generation pipeline:
 - If the keywords are not able to find any enzyme category with high match count, try using more flexible keywords (synonyms, break words, etc.) or ask the user for more information
 - The mined motifs sometimes only differ in their coordinates, try to extract the motifs one at a time and only moving on if the previous one did not generate any good results downstream, like low binding affinity with the substrate
 If the user asks for running a simulation system, use the gromacs copilot tool and directly figure out the prompt from the user request. If the user request misses any parameters, use the default ones without asking them.
 If the user asks for running a docking and provide ligand and receptor files, use the docking pipeline. Ligand files need to be in .sdf format and receptor needs to be in .pdb. Infer receptor name and ligand names based on the provided file names. If the user request misses any parameters, use the default ones without asking them.
 if the user provided file format does not match the docking pipeline input, use the substrate conversion tool. Infer input format based on the provided file name. If the user request misses any parameters, use the default ones without asking them.
+
+The heme binder pipeline follows this sequence: Ensure combined protein and ligand PDB is provided -> Separate the ligand file if ligand pdb/sdf/mol file not separately provided -> Ensure params file and CST file are created -> Run RF Diffusion -> Analyze RF Diffusion Outputs -> Run ProteinMPNN -> Run AF2 -> Analyze AF2 Outputs -> Run LigandMPNN -> Analyze LigandMPNN Outputs -> Run LigandMPNN on 2nd layer residues -> Run AF2 -> Analyze AF2 Outputs -> Run FastRelax -> Analyze FastRelax Outputs
+At any step, if the model does not meet the required criteria (e.g., SASA, RMSD, LDDT, etc.), try rerunning the previous stages with improved parameters.
+Do not continue the pipeline with number of good structure(s) being 0 (try making thresholds more flexible if required).
 
 User request: {query}
 """
